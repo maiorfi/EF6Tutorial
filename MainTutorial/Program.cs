68d43efe662cf5bf99ee9ba358c65a2caf5dec92
Lorenzo Maiorfi <maiorfi@gmail.com>
2022-01-04 09:40:10 +0100

    test 1

--- MainTutorial/Program.cs
+++ MainTutorial/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace MainTutorial
+﻿using System;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainTutorial
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
+            await runTest1();
+        }
+
+        private async static Task runTest1()
+        {
+            using (var ctx = new SchoolDBEntities())
+            {
+                var student = await ctx.Students
+                    .Where(s => s.StudentName == "Bill")
+                    .FirstOrDefaultAsync();
+
+                Console.WriteLine(ObjectDumper.Dump(student));
+
+                Debug.Assert(student != null);
+            }
         }
     }
 }
